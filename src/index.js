a333b30d991c9dec2458926d3cc11fa6d1ceb553
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-07-24 16:04:53 +0100

    background and splittext added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import gsap from "gsap";
-import "./scrolling";
+import scrolling from "./scrolling";
 
 const showPage = () => {
-  document.onreadystatechange = (e) => console.log("Ready", e);
+  // split-text
+  document.querySelectorAll(".split-text").forEach(
+    (holder) =>
+      (holder.innerHTML = holder.innerHTML
+        .split(" ")
+        .filter((word) => word !== "")
+        .map((word) => '<span class="split-word">' + word + "</span>")
+        .join(""))
+  );
+  scrolling();
 };
 
-showPage();
+document.onreadystatechange = (e) => {
+  if (document.readyState == "complete") {
+    showPage();
+  }
+  console.log("Ready", e);
+};
--- src/scrolling.js
+++ src/scrolling.js
@@ -1,3 +1,56 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Scrollbar from "smooth-scrollbar";
 
-Scrollbar.init(document.querySelector(".smooth-scroll-wrapper"));
+const scrolling = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  let scrollbar = Scrollbar.init(
+    document.querySelector(".smooth-scroll-wrapper")
+  );
+
+  ScrollTrigger.scrollerProxy(document.body, {
+    scrollTop(value) {
+      if (arguments.length) {
+        scrollbar.scrollTop = value;
+      }
+      return scrollbar.scrollTop;
+    },
+  });
+
+  // Reveal Background Image
+  var tl = gsap.timeline({ ease: "slow (0.7 0.7, false)" });
+  tl.set(".smooth-scroll-wrapper", { opacity: 1, duration: 0 }).from(
+    ".items-area",
+    {
+      "--coverHeight": "100%",
+      translateY: "100%",
+      duration: 1,
+      delay: "0.5s",
+    }
+  );
+  gsap.to(".split-word", {
+    scrollTrigger: {
+      trigger: ".split-word",
+      scroller: document.body,
+      start: "center bottom",
+    },
+    "--wordCoverHeight": "0%",
+    stagger: 0.1,
+    duration: 0.7,
+    delay: 0.5,
+    ease: "expo.out",
+  });
+
+  gsap.from("img", {
+    scrollTrigger: {
+      trigger: "img",
+      scroller: document.body,
+      start: "center bottom",
+    },
+    delay: 0.5,
+    opacity: 0,
+  });
+};
+
+export default scrolling;
